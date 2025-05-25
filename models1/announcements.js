e137d75005e3f688462bee6a8f7c05cf4b8572ab
vansh2948 <vsachdeva514@gmail.com>
2025-05-25 20:22:43 +0530

    Update announcements.js

--- models1/announcements.js
+++ models1/announcements.js
@@ -1,8 +1,16 @@
+
 const mongoose = require('mongoose');
 
 const announcementSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  text: {
+    type: String,
+    required: true,
+    trim: true // Removes leading/trailing spaces
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Announcement', announcementSchema);
