46d71779fae27eb34eac568d645f3cc9c1aaffac
vansh2948 <vsachdeva514@gmail.com>
2025-05-25 12:48:54 +0530

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  message: String
-});
-
-module.exports = mongoose.model('Contact', contactSchema);
-
-const mongoose = require('mongoose');
-
-const contactSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  message: String
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
